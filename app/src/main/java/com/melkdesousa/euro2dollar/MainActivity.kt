faf5bb428c289fa533e73366f76e1f2093e2a3e6
Melk de Sousa <melkdesousa@pop-os.localdomain>
2023-12-23 19:25:41 -0300

    Initial commit

---
+++ app/src/main/java/com/melkdesousa/euro2dollar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.melkdesousa.euro2dollar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
